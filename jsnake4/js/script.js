b5f31291d60fb972af8dbe3b0ca539ec8ebd302c
KOVU9529 <bapgiordano@gmail.com>
2022-06-09 15:50:47 +0200

    jsnake4

---
+++ jsnake4/js/script.js
@@ -0,0 +1,32 @@
+/*Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.*/
+
+//Creo un array vuoto e controllo
+
+const array=[];
+console.log(array);
+
+//La variabile del prompt
+
+let answer;
+
+//imposto le condizioni del ciclo
+
+for(let i=0; i<6; i++){
+
+    //Coversione risposta prompt in numero e controllo
+    answer=parseInt(prompt('Dammi un numero'));
+    console.log(answer);
+
+    //Imposto le condizioni per l'inserimento numerico nell'array
+
+    if(answer % 2 === 0){
+        array.push(answer);
+    } else{
+    }
+}
+
+//Controllo finale
+
+console.log(array);
